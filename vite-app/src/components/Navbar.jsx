f6255b2223521cc8df617265fdcb2243ebb22651
RaveeshSam <raveesh.s@iexcel.in>
2024-04-15 19:59:16 +0530

    Added: Navbar

---
+++ vite-app/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Gallery</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
