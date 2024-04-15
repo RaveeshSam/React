d16bd05f615a9941b45e7467cac55c435bf6c96e
RaveeshSam <raveesh.s@iexcel.in>
2024-04-15 20:00:51 +0530

    Added: Footer

---
+++ vite-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        Copyright &copy; Raveesh Samkaria || All Rights Reserved
+    </div>
+  )
+}
+
+export default Footer
