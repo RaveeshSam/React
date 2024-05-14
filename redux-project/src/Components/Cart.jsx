d78c63780a2fd8a28cfd51903ced1fe90a72176b
RaveeshSam <raveesh.s@iexcel.in>
2024-05-14 10:06:51 +0530

    Added: Part III - E-Commerce Project with Redux

--- redux-project/src/Components/Cart.jsx
+++ redux-project/src/Components/Cart.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {remove} from '../Redux/cartSlice'
 
 const Cart = () => {
+  const cartItems = useSelector((state) => state.cart)
+  const dispatch = useDispatch()
+
+  const handleRemove = (id) => {
+    dispatch(remove(id))
+  }
+
   return (
-    <div>Cart</div>
+    <div>
+      {
+        cartItems.map((item) => (
+          <div className='cart' key={item.id}>
+            <img src={item.image} alt="" />
+            <h4>{item.title}</h4>
+            <h5>{item.price}</h5>
+            <button className='rbtn' onClick={() => handleRemove(item.id)}>Remove</button>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
